e434f220086508b69c2b4d233c0d291d7ec2b4f0
Pierre-Alexandre St-jean <pa@stjean.me>
2011-03-05 01:18:56 -0500

    Good rake task

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,9 @@
 mkdir ~/src
 
-## init rvm
-#bash < <( curl http://rvm.beginrescueend.com/releases/rvm-install-head )
-#source "$HOME/.rvm/scripts/rvm"
-#rvm install 1.9.2
-#rvm use 1.9.2 --default
 cd ~/src
 git clone git@github.com:pastjean/dotfiles.git
 cd dotfiles
-rake install
-## init hg
-## init node / npm
-#echo "Installing node & npm"
-#cd src
-#git clone git://github.com/joyent/node.git node
-#cd node
-#./configure --prefix=~/lib/node
-#make install
-#cd ..
-#curl http://npmjs.org/install.sh | sh
+rake
 
 ## init scala
 ## init clojure
