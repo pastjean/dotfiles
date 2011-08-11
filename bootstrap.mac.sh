30b3b7582dc6c55b820bf84738fa5bdbdd6221fc
Pierre-Alexandre St-jean <pa@stjean.me>
2011-08-11 19:43:39 -0400

    mac bootstrap.sh

---
+++ bootstrap.mac.sh
@@ -0,0 +1,7 @@
+# install homebrew in /usr/local
+/usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
+# there is alternative
+curl https://github.com/mxcl/homebrew/tarball/master | tar -xvf
+
+brew install wget
+
