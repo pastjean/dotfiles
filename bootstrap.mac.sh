21ca147194338e2754f302b10f6b4e8e626ec2be
Pierre-Alexandre St-jean <pa@stjean.me>
2011-09-08 20:52:33 -0400

    simpler mac bootstrapping

--- bootstrap.mac.sh
+++ bootstrap.mac.sh
@@ -1,22 +1,16 @@
 #!/bin/sh
 
-DIR=$HOME/.homebrew
+DIR=$HOME/.brew
 mkdir -p $DIR
 curl -L https://github.com/mxcl/homebrew/tarball/master | tar xvz --strip 1 -C $DIR
 
-
-export PATH=~/$DIR/bin:$PATH
-
+echo 'export PATH=$DIR/bin:$PATH' >> .localrc
+export PATH=$DIR/bin:$PATH
 
 brew install wget
-brew install git
-# brew install macvim
+
 # closure-compiler
 # android-sdk
 # android-ndk
-# clojure
-# clojure-contrib
 # ctags
 # rlwrap
-
-echo "add $DIR/bin to your path"
