81a2f3e2985f3c7b4f859c7ed931deacd2d04fb0
Pierre-Alexandre St-jean <pa@stjean.me>
2011-09-08 20:53:06 -0400

    real half bin/pass ; ruby bootstraper user rbenv/rbbuild

--- bootstrap.ruby.sh
+++ bootstrap.ruby.sh
@@ -1,6 +1,14 @@
 echo "Installing Ruby 1.9.2 stable and making it the default Ruby ..."
-  rvm install 1.9.2-p290
-  rvm use 1.9.2 --default
+  git clone git://github.com/sstephenson/rbenv.git .rbenv
+
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> .localrc
+  echo 'eval "$(rbenv init -)"' >> .localrc
+  source .localrc
+
+  ruby-build 1.9.2-p290
+  rbenv rehash
+  rbenv global 1.9.2-p290
 
 echo "Installing Bundler, Heroku, Chef, lolcat(for fun)"
   gem install bundler heroku chef lolcat --no-rdoc --no-ri
+
