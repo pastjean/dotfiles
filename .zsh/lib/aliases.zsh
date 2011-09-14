308867c28222da6c5c9fbf768f897447dc21816f
Pierre-Alexandre St-jean <pa@stjean.me>
2011-09-13 22:02:55 -0400

    integration of parts of oh-my-zsh

---
+++ .zsh/lib/aliases.zsh
@@ -0,0 +1,14 @@
+# Directory operations
+alias ..='cd ..'
+alias cd..='cd ..' # for typos
+alias ...='cd ../..'
+alias -- -='cd -' # get to last directory
+
+# Super user
+alias _='sudo'
+
+# Show history
+alias history='fc -l 1'
+
+#ls
+alias sl=ls # screw you typos
--- .zsh/lib/git.zsh
+++ .zsh/lib/git.zsh
@@ -1,76 +1,64 @@
-# From there http://sebastiancelis.com/2009/nov/16/zsh-prompt-git-users/
-# and there http://gist.github.com/244215
-# Modified to extract formating and color directly in prompt definition
+# get the name of the branch we are on
+function git_prompt_info() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+}
 
-function git_update_current_vars {
-unset __CURRENT_GIT_BRANCH
-unset __CURRENT_GIT_BRANCH_STATUS
-unset __CURRENT_GIT_BRANCH_IS_DIRTY
-
-local st="$(git status 2>/dev/null)"
-if [[ -n "$st" ]]; then
-  local -a arr
-  arr=(${(f)st})
-
-  if [[ $arr[1] =~ 'Not currently on any branch.' ]]; then
-    __CURRENT_GIT_BRANCH='no-branch'
+# Checks if working tree is dirty
+parse_git_dirty() {
+  if [[ -n $(git status -s 2> /dev/null) ]]; then
+    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
   else
-    __CURRENT_GIT_BRANCH="${arr[1][(w)4]}";
+    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
   fi
-
-  if [[ $arr[2] =~ 'Your branch is' ]]; then
-    if [[ $arr[2] =~ 'ahead' ]]; then
-      __CURRENT_GIT_BRANCH_STATUS='ahead'
-    elif [[ $arr[2] =~ 'diverged' ]]; then
-      __CURRENT_GIT_BRANCH_STATUS='diverged'
-    else
-      __CURRENT_GIT_BRANCH_STATUS='behind'
-    fi
-  fi
-
-  if [[ ! $st =~ 'nothing to commit' ]]; then
-    __CURRENT_GIT_BRANCH_IS_DIRTY='1'
-  fi
-fi
 }
 
-function git_prompt_info {
-if [ -n "$__CURRENT_GIT_BRANCH" ]; then
-  local s="$__CURRENT_GIT_BRANCH"
-  case "$__CURRENT_GIT_BRANCH_STATUS" in
-    ahead)
-      s+="↑"
-      ;;
-    diverged)
-      s+="↕"
-      ;;
-    behind)
-      s+="↓"
-      ;;
-  esac
-  if [ -n "$__CURRENT_GIT_BRANCH_IS_DIRTY" ]; then
-    s+="⚡"
+# Checks if there are commits ahead from remote
+function git_prompt_ahead() {
+  if $(echo "$(git log origin/$(current_branch)..HEAD 2> /dev/null)" | grep '^commit' &> /dev/null); then
+    echo "$ZSH_THEME_GIT_PROMPT_AHEAD"
   fi
-
-  printf "%s" $s
-fi
 }
 
-function preexec_git_update_vars {
-case "$1" in
-  git*)
-    __EXECUTED_GIT_COMMAND=1
-    ;;
-esac
+# Formats prompt string for current git commit short SHA
+function git_prompt_short_sha() {
+  SHA=$(git rev-parse --short HEAD 2> /dev/null) && echo "$ZSH_THEME_GIT_PROMPT_SHA_BEFORE$SHA$ZSH_THEME_GIT_PROMPT_SHA_AFTER"
 }
 
-function precmd_git_update_vars {
-if [ -n "$__EXECUTED_GIT_COMMAND" ]; then
-  git_update_current_vars
-  unset __EXECUTED_GIT_COMMAND
-fi
+# Formats prompt string for current git commit long SHA
+function git_prompt_long_sha() {
+  SHA=$(git rev-parse HEAD 2> /dev/null) && echo "$ZSH_THEME_GIT_PROMPT_SHA_BEFORE$SHA$ZSH_THEME_GIT_PROMPT_SHA_AFTER"
 }
 
-function chpwd_git_update_vars {
-  git_update_current_vars
+# Get the status of the working tree
+git_prompt_status() {
+  INDEX=$(git status --porcelain 2> /dev/null)
+  STATUS=""
+  if $(echo "$INDEX" | grep '^?? ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_UNTRACKED$STATUS"
+  fi
+  if $(echo "$INDEX" | grep '^A  ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_ADDED$STATUS"
+  elif $(echo "$INDEX" | grep '^M  ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_ADDED$STATUS"
+  fi
+  if $(echo "$INDEX" | grep '^ M ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
+  elif $(echo "$INDEX" | grep '^AM ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
+  elif $(echo "$INDEX" | grep '^ T ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
+  fi
+  if $(echo "$INDEX" | grep '^R  ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_RENAMED$STATUS"
+  fi
+  if $(echo "$INDEX" | grep '^ D ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_DELETED$STATUS"
+  elif $(echo "$INDEX" | grep '^AD ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_DELETED$STATUS"
+  fi
+  if $(echo "$INDEX" | grep '^UU ' &> /dev/null); then
+    STATUS="$ZSH_THEME_GIT_PROMPT_UNMERGED$STATUS"
+  fi
+  echo $STATUS
 }
