ae434685e8b96fe1a2bc3c6c9b7d76b20b1636f0
Pierre-Alexandre St-Jean <pierrealexandre.stjean@gmail.com>
2012-06-03 09:54:16 -0400

    corrected some mistakes in install script

--- bin/pass.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-sha1=`echo -n $1 | openssl dgst -sha1`
-base64=`echo -n $1 | openssl sha1 -binary | openssl base64`
-
-echo "SHA1:       " $sha1
-echo "SHA1-Half:  " `echo -n $sha1 | cut -c -20`
-echo "Base64:     " $base64
-echo "Base64-Half:" `echo -n $base64 | cut -c -14`
--- tools/bootstrap.clojure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Bootstraping clojure"
-curl https://raw.github.com/technomancy/leiningen/stable/bin/lein > ~/bin/lein
-chmod +x ~/bin/lein
-
-
