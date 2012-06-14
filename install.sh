433bdc50f72cb0a5bb8f6bb7c1a4888888d2ebfe
Pierre-Alexandre St-Jean <pierrealexandre.stjean@gmail.com>
2012-06-14 08:27:09 -0400

    If git-prompt-status is not there define it empty

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "$0" )/.." && pwd )"
+DIR="$( cd $( dirname $0 ) && pwd )"
 DESTINATION=$HOME
 
+echo $DIR
 #Fish Special configuration
 FISHDEST=$DESTINATION/.config/fish
 mkdir -p $FISHDEST
-ln -s $DIR/fish/config.fish $FISHDEST/config.fish
+ln -s $DIR/fish/config.fish $HOME/.config/fish/config.fish
