c28d97577c0506fdb92604247d98c647851585a8
Pierre-Alexandre St-Jean <pierrealexandre.stjean@gmail.com>
2012-06-13 23:45:08 -0400

    OH fish is my new fuckin awesome shell'

---
+++ fish/config.fish
@@ -0,0 +1,72 @@
+set fish_greeting
+set EDITOR vim
+
+alias g 'git'
+alias v 'vim'
+alias o 'open'
+alias j 'z'
+
+alias pbc 'pbcopy'
+alias pbp 'pbpaste'
+
+
+set PATH "$HOME/bin" $PATH
+set PATH "$HOME/local/bin" $PATH
+set PATH "$HOME/.brew/bin" $PATH
+set PATH "$HOME/.rbenv/bin" $PATH
+
+
+set BROWSER open
+
+function git_prompt
+  if git rev-parse --show-toplevel >/dev/null 2>&1
+    set_color normal
+    printf ' on %s ' \u2b60
+    set_color magenta
+    printf '%s' (git currentbranch ^/dev/null)
+    set_color green
+#git_prompt_status
+    set_color normal
+  end
+end
+
+function fish_prompt
+  set last_status $status
+
+  set_color magenta
+  printf '%s' (whoami)
+  
+  set_color normal
+  printf ' at '
+  set_color yellow
+  printf '%s' (hostname|cut -d . -f 1)
+  
+  set_color normal
+  printf ' in '
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
+  set_color normal
+
+  git_prompt
+
+  echo
+
+  if test $last_status -eq 0
+    set_color white -o
+    printf '><((°> '
+  else
+    set_color red -o
+    printf '[%d] ><((ˣ> ' $last_status
+  end
+
+  set_color normal
+end
+
+
+if test -s $HOME/.config/fish/local.fish
+      source $HOME/.config/fish/local.fish
+end
+
+if status --is-interactive
+  fortune -s | lolcat
+end
--- install.sh
+++ install.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "$0" )/.." && pwd )"
-DEST_DIR=$HOME
+DESTINATION=$HOME
 
-if [ "$1" == "test" ];then
-  echo "testing......"
-  DEST_DIR=$DIR/test_install_script
-  mkdir $DEST_DIR
-fi
-
-mkdir -p $DEST_DIR/bin
-
-cd $DIR
-FILES_TO_LINK=$(ls -A | grep -v -e ".git" -e ".gitignore" -e "tools" -e "README.md" -e "DS_Store" -e "bin" ; ls -A bin/*)
-
-
-# Link files
-for file in $FILES_TO_LINK
-do
- if [[ $(find -L $DEST_DIR -maxdepth 1 -samefile $file -print) != "" ]]
- then
-    echo "identical $file"
-  else
-    echo "link $file to $DEST_DIR/$file"
-    ln -nis $DIR/$file $DEST_DIR/$file
- fi;
-done
+#Fish Special configuration
+FISHDEST=$DESTINATION/.config/fish
+mkdir -p $FISHDEST
+ln -s $DIR/fish/config.fish $FISHDEST/config.fish
