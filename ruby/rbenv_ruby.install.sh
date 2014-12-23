ee64272d98ba18998b79ddedb7232fa7a7c23c56
Pierre-Alexandre St-jean <pa@stjean.me>
2014-12-23 14:51:19 -0500

    update

--- ruby/rbenv_ruby.install.sh
+++ ruby/rbenv_ruby.install.sh
@@ -3,28 +3,9 @@
 # rbenv
 git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
 
-cat << EOF >> "$HOME/.localrc"
-# Set rbenv in path
-export PATH="$HOME/.rbenv/bin:$PATH"
-# Make a cache trick to make shell load a little faster
-rbenv_cache="$HOME/.rbenv-init-zsh"
-if [ "$(command -v rbenv)" -nt "$rbenv_cache" -o ! -s "$rbenv_cache" ]; then
-    rbenv init - >| "$rbenv_cache"
-fi
-source "$rbenv_cache"
-unset rbenv_cache
-EOF
-
 # ruby-build
-mkdir -p ~/.rbenv/plugins
-cd ~/.rbenv/plugins
-git clone git://github.com/sstephenson/ruby-build.git
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
-# oh my zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-chsh -s /bin/zsh
+# add rbenv to shell (copy the file but this must be coded)
 
-echo "Restart your shell buddy"
-
-/usr/bin/env zsh
-source ~/.zshrc
+echo "copy addtolocalrc.sh to $HOME/.localrc"
--- zsh/ohmyzsh.install.sh
+++ zsh/ohmyzsh.install.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+chsh -s /bin/zsh
+
+echo "Restart your shell buddy"
+
+/usr/bin/env zsh
+source ~/.zshrc
