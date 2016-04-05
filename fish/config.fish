1f94a21e6518b516c535808c53ba417a6ad20391
Pierre-Alexandre St-Jean <pastjean@coveo.com>
2016-04-05 17:48:34 -0400

    fish fun

--- fish/config.fish
+++ fish/config.fish
@@ -2,29 +2,26 @@ set FISH_RC (readlink $HOME/.config/fish/config.fish)
 set FISH_TOPIC_DIR (dirname $FISH_RC)
 set DOTFILES (dirname $FISH_TOPIC_DIR)
 
-# Path
-# --------------------
-
-function prepend_to_path -d "Prepend the given dir to PATH if it exists and is not already in it"
-    if test -d $argv[1]
-        if not contains $argv[1] $PATH
-            set -gx PATH "$argv[1]" $PATH
-        end
-    end
-end
-
-prepend_to_path "$HOME/bin"
-prepend_to_path "$HOME/local/bin"
-prepend_to_path "$HOME/.brew/bin"
-prepend_to_path "$HOME/.rbenv/shims"
-prepend_to_path "$HOME/.rbenv/bin"
-prepend_to_path "$DOTFILES/bin" $PATH
-
-# Aliases
-# --------------------
 
 function ..;    cd ..; end
 function ...;   cd ../..; end
+function ....;  cd ../../..; end
+function .....; cd ../../../..; end
+
+# I give up
+alias :q exit
+
+
+# Completions
+
+function make_completion --argument alias command
+    complete -c $alias -xa "(
+        set -l cmd (commandline -pc | sed -e 's/^ *\S\+ *//' );
+        complete -C\"$command \$cmd\";
+    )"
+end
+
+make_completion g "git"
 
 # Basic Config
 # --------------------
@@ -40,12 +37,9 @@ function fish_user_key_bindings
     jesus_fucking_christ_bind_the_fucking_keys_fish
 end
 
-set fish_greeting ''
-set EDITOR vim
-set BROWSER open
 
 function __fish_method_missing --on-event fish_command_not_found
-  echo "WHATCHU TYPING ---> $argv"
+#  echo "WHATCHU TYPING ---> $argv"
 end
 
 # for FISHFILE in (ls $DOTFILES/**/*.fish)
@@ -57,14 +51,6 @@ end
 # Source all .fish files in dotfile dir
 
 
-# Set local configs on
-# --------------------
-
-set localconfigs "$HOME/.config/fish/local.fish" "$HOME/.local.fish"
-if test -f "$HOME/.config/fish/local.fish"
-      . $HOME/.config/fish/local.fish
-end
-
 
 # Plugins dir (ala oh-my-zsh)
 # ---------------------------
@@ -72,6 +58,32 @@ end
 for plugin in (find $FISH_TOPIC_DIR/plugins -name "*.fish")
   . $plugin
 end
+
+# Env vars
+# --------------------
+
+
+function prepend_to_path -d "Prepend the given dir to PATH if it exists and is not already in it"
+    if test -d $argv[1]
+        if not contains $argv[1] $PATH
+            set -gx PATH "$argv[1]" $PATH
+        end
+    end
+end
+
+prepend_to_path "$HOME/bin"
+prepend_to_path "$HOME/local/bin"
+prepend_to_path "$HOME/.brew/bin"
+prepend_to_path "$DOTFILES/bin"
+prepend_to_path "$HOME/.fzf/bin/"
+ 
+set -g -x GOPATH "$HOME/go"
+prepend_to_path "$HOME/go/bin"
+
+set -g -x fish_greeting ''
+set -g -x EDITOR vim
+set BROWSER open
+
 # Prompt
 # --------------------
 function prompt_pwd --description 'Print the current working directory, shortend to fit the prompt'
@@ -88,7 +100,7 @@ function to_human_time -d "Turn a timestamp (in s) to a readable time 165392 => 
   set hours (math "$tmp / 60 / 60 % 24")
   set minutes (math "$tmp /60 % 60")
   set seconds (math "$tmp % 60")
-  
+
   set str ""
   test "$days" -gt "0"; and set str "$str "$days"d"
   test "$hours" -gt "0"; and set str "$str "$hours"h"
@@ -111,23 +123,23 @@ function fish_prompt
   set last_status $status
 
   if test "$SSH_CONNECTION" != ""
-    printf "%s@%s" (whoami) (hostname|cut -d . -f 1)
+    printf "%s@%s " (whoami) (hostname|cut -d . -f 1)
   end
-  
-  # fasd -A "$PWD"
+
+  fasd -A "$PWD"
     if test $last_status -eq 0
-        set_color magenta -o
-        printf '❯ '
+        set_color green -o
+        printf 'λ  '
     else
         set_color red -o
-        printf '❯ ' $last_status
+        printf '😱  ' $last_status
     end
-  
+
 end
 
-
-if status --is-interactive
-#  fortune -s | lolcat
+# Local settings
+if test -f $HOME/.local.fish
+    . $HOME/.local.fish
 end
 
 true
