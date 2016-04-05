795b813bc48c2220e9a9835c087a9380c43be263
Pierre-Alexandre St-Jean <pastjean@coveo.com>
2016-04-05 18:00:19 -0400

    fish fish fish

--- dev-tools/dev-tools.install.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-echo "More tools $DOTFILES_DIR/dev-tools/dev-tools"
--- zsh/ohmyzsh.install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-
-chsh -s /bin/zsh
-
-echo "Restart your shell buddy"
-
-/usr/bin/env zsh
