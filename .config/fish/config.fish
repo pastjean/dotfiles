bc5bfd641bf5c61463f022d970dbb877cf4fbd40
Pierre-Alexandre St-Jean <pa@stjean.me>
2020-08-06 12:07:12 -0400

    fasd to zoxide

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -106,19 +106,7 @@ if test -f $HOME/.local.fish
     . $HOME/.local.fish
 end
 
-# fasd
-# --------------
-if test -e (which fasd)
-  function _run_fasd -e fish_preexec
-    fasd --proc (fasd --sanitize "$argv") > "/dev/null" 2>&1
-  end
-
-  function z
-    cd (fasd -d -l -1 "$argv")
-  end
-else
-  echo "🍒  Please install 'fasd' first!"
-end
+zoxide init fish | source
 
 # osx
 # --------------
