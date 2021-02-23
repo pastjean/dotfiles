5aa5789802ce412c35d8251a14a3cc11460c4e19
Pierre-Alexandre St-Jean <pa@stjean.me>
2021-02-23 18:50:15 -0500

    !

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -37,23 +37,17 @@ set DOTFILES (dirname (dirname $FISH_TOPIC_DIR))
 set -g fish_function_path "$FISH_TOPIC_DIR/functions" $fish_function_path
 
 set -g -x PATH "$HOME/bin" $PATH
-set -g -x PATH "$HOME/.brew/bin" "$HOME/.brew/sbin" $PATH
 set -g -x PATH "$DOTFILES/bin" $PATH
 
 set -g -x PATH "$HOME/.fzf/bin/" $PATH
 set -g -x PATH "$HOME/go/bin" $PATH
 set -g -x PATH "$HOME/.cargo/bin" $PATH
-set -g -x PATH "$HOME/.rbenv/shims" $PATH
 
 set -g -x fish_greeting ''
 set -g -x EDITOR vim
 set -g -x GPG_TTY (tty)
 
 set BROWSER open
-
-zoxide init fish | source
-starship init fish | source
-status --is-interactive; and source (rbenv init -|psub)
 
 # Local settings
 # --------------
