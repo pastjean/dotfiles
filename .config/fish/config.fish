a56ddbb9657c8ce57fd94e100ed9bf20f2a9938b
Pierre-Alexandre St-Jean <pa@stjean.me>
2025-02-21 14:18:28 -0500

    !!

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -58,8 +58,21 @@ if test -f $HOME/.local.fish
 end
 
 set -g -x PATH /opt/homebrew/bin $PATH
+/opt/homebrew/bin/brew shellenv | source
 
 starship init fish | source
 zoxide init fish | source
-/opt/homebrew/bin/brew shellenv | source
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
